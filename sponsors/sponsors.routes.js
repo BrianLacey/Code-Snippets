0d99281b54db44e3f46ed2aab634f654e7dfe912
Brian Lacey <38966475+BrianLacey@users.noreply.github.com>
2018-05-06 14:42:30 -0700

    Update sponsors.routes.js

--- sponsors/sponsors.routes.js
+++ sponsors/sponsors.routes.js
@@ -1,3 +1,8 @@
+/* Sponsor API route definitions utilizing Express.js running on Node.js.
+validateBody middleware functions and schema are implementations of Joi.js.
+Route definitions are kept separate from controller and service layers,
+adhering to best practices of clean, tidy code and single responsibility principle. */
+
 const router = require('express').Router();
 const sponsorsController = require('../controllers/sponsors.controller');
 const validateBody = require('../filters/validate.body');
